e7003940f45d11bed8e3fe7e23a69def5ab8c419
romamaxime <roma.maxime1994@gmail.com>
2017-03-28 12:02:01 +0200

    add .css() for background-color

--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 	$("#removeDiv").on('click',function(){
 		$("#remove").remove();
 	});
-
-
 	
 	var str = "";
 	$("#setText").on("keypress",function(){		
 		str = $("#setText").val();
 		$("#needText").text(str);
+	});
+
+	$("#setColor").click(function(){
+		$(".yellow").css("background-color", "red");
 	});
 });
