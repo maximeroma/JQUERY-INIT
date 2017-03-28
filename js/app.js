f0e8f45c46a6b29002851419198e33b753da01ac
romamaxime <roma.maxime1994@gmail.com>
2017-03-28 11:57:43 +0200

    add return value of input

--- js/app.js
+++ js/app.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
 	$("#removeDiv").on('click',function(){
 		$("#remove").remove();
 	});
+
+
+	
+	var str = "";
+	$("#setText").on("keypress",function(){		
+		str = $("#setText").val();
+		$("#needText").text(str);
+	});
 });
